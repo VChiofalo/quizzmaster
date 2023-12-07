5d73d0fb70f65cd5dab18b2d976b69828381a7f5
Fredericvamer <frederic.vamer@epsi.fr>
2023-12-07 17:08:55 +0100

    modif question

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -1,16 +1,42 @@
-import  { useState } from 'react';
+import  { useState, useEffect } from 'react';
 import './style.css';
 
 function Question() {
+  const [questions, setQuestions] = useState([]);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+
+  useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=10&difficulty=medium&type=boolean')
+      .then(response => response.json())
+      .then(data => {
+        setQuestions(data.results);
+      })
+      .catch(error => console.error('Erreur lors de la récupération des questions', error));
+  }, []);
 
   const handleAnswerChange = (value) => {
     setSelectedAnswer(value);
   };
 
+  function handleNextQuestion() {
+    setCurrentQuestionIndex(prevIndex => {
+      // Vérifier si c'est la dernière question
+      if (prevIndex === questions.length - 1) {
+        // Peut-être rediriger vers une page de résultats ou afficher un message
+        console.log('Quiz terminé');
+        return prevIndex;
+      }
+      return prevIndex + 1;
+    });
+  }
+
+
   return (
     <div className="quiz-container">
-      <h1>Question   ?</h1>
+      <h1>Quizmaster</h1>
+      <p>{questions.question}</p>
       <div className="options">
         <label>
           <input
@@ -33,7 +59,7 @@ function Question() {
           Faux
         </label>
       </div>
-      <button >Soumettre</button>
+      <button onClick={handleNextQuestion}>Valider</button>
     </div>
   );
 }
