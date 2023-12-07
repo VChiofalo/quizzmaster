ea77ae27040078ff26304d992e9428c0d6ba2bf3
Cisco29 <castro.francisco@live.fr>
2023-12-07 22:48:30 +0100

    tentative neon light buttom
    on verra demain pour le bouton "commencez le quiz"

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,33 +3,33 @@ import { Link } from 'react-router-dom';
 import styles from './Home.module.css';
 import { useEffect, useRef } from 'react';
 const Home = () => {
+  const colorText = useRef(null);
 
-    const colorText = useRef(null)
   useEffect(() => {
     // Code JavaScript pour l'effet de couleur
-    
-    colorText.current.innerHTML = colorText.current.innerText.split('')
+    colorText.current.innerHTML = colorText.current.innerText
+      .split('')
       .map(
         (letters, i) =>
           `<span style="transition-delay:${i * 40}ms;filter:hue-rotate(${i * 30}deg)">${letters}</span>`
       )
       .join('');
-  }, []); // Assurez-vous d'ajouter les dépendances vides pour exécuter le code une seule fois lors du montage initial
+  }, []);
 
   return (
     <div className={styles.container}>
       <h1 className={styles.text} ref={colorText}>
-        <span  className={styles.text}>Welcome</span> Quizz Master
+        <span className={styles.text}>Welcome</span> Quiz Master
       </h1>
       <p>
         Bienvenue sur notre quizz réalisé en React avec Vincent, Fred, Erwan, et Cisco.
         Découvrez des questions passionnantes et testez vos connaissances !
       </p>
-      {/* Ajoutez la balise img avec le chemin de votre image */}
-      <img src='/public/quizMaster.png' alt="Description de l'image" className={styles.yourImageClass} />
+      <img src='/public/quizMaster.png' alt="Description de l'image" className={`${styles.yourImageClass} ${styles.leftShift}`} />
       <div className={styles.startButtonContainer}>
         <Link to="/question/1">
-          <button>Commencer le quizz</button>
+          {/* Ajoutez la classe neonButton au bouton */}
+          <button className={`${styles.neonButton}`}>Commencer le Quiz</button>
         </Link>
       </div>
     </div>
