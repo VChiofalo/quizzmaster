174691f186a23b293a7731d079ef8100a1f721ec
VChiofalo <117775290+VChiofalo@users.noreply.github.com>
2023-12-07 16:20:55 +0100

    Results

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Results from './components/Results/Results'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Results />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
