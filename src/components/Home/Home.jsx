d58dfce8f96dabcaa8b33fd3058ac5b7330ea4bc
Cisco29 <castro.francisco@live.fr>
2023-12-07 18:10:33 +0100

    css et javascript
    tentative de mettre un effet sur "welcome quizz master" manque le hover

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,31 @@
-//import React from 'react';
+//import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import styles from './Home.module.css';
+import { useEffect, useRef } from 'react';
 const Home = () => {
+
+    const colorText = useRef(null)
+  useEffect(() => {
+    // Code JavaScript pour l'effet de couleur
+    
+    colorText.current.innerHTML = colorText.current.innerText.split('')
+      .map(
+        (letters, i) =>
+          `<span style="transition-delay:${i * 40}ms;filter:hue-rotate(${i * 30}deg)">${letters}</span>`
+      )
+      .join('');
+  }, []); // Assurez-vous d'ajouter les dépendances vides pour exécuter le code une seule fois lors du montage initial
+
   return (
-    <div className="home-container">
-      <h1>Welcome Quizz Master</h1>
+    <div className={styles.container}>
+      <h1 className={styles.text} ref={colorText}>
+        <span  className={styles.text}>Welcome</span> Quizz Master
+      </h1>
       <p>
         Bienvenue sur notre quizz réalisé en React avec Vincent, Fred, Erwan, et Cisco.
         Découvrez des questions passionnantes et testez vos connaissances !
       </p>
-      <div className="start-button-container">
-        {/* Utilisation de Link pour créer un lien vers la première question du quizz */}
+      <div className={styles.startButtonContainer}>
         <Link to="/question/1">
           <button>Commencer le quizz</button>
         </Link>
