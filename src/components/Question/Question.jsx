9633df0801f92a4a591e5ce662ef705fe99e26cb
VChiofalo <117775290+VChiofalo@users.noreply.github.com>
2023-12-08 12:09:14 +0100

    Algo quizz Erwan

--- src/components/Question/Question.jsx
+++ src/components/Question/Question.jsx
@@ -5,81 +5,60 @@ function Question() {
   const [questions, setQuestions] = useState([]);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
 
 
   useEffect(() => {
-    // fetch('https://opentdb.com/api.php?amount=10&difficulty=medium&type=boolean')
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     console.log(data.results[0].question)
-    //     setQuestions(data.results);
-    //   })
-    //   .catch(error => console.error('Erreur lors de la récupération des questions', error));
-
     const fetchData = async () => {
       const response = await fetch('https://opentdb.com/api.php?amount=10&difficulty=medium&type=boolean');
-      const data = await response.json()
-      console.log(data)
-      setQuestions(data.results)
+      const data = await response.json();
+      setQuestions(data.results);
     }
-
     fetchData()
-
-    
   }, []);
-
-  const handleAnswerChange = (value) => {
-    setSelectedAnswer(value);
-  };
 
   function handleNextQuestion() {
     setCurrentQuestionIndex(prevIndex => {
       // Vérifier si c'est la dernière question
       if (prevIndex === questions.length - 1) {
         // Peut-être rediriger vers une page de résultats ou afficher un message
-        console.log('Quiz terminé');
+        <Link to= state={{score: score}}></Link>
         return prevIndex;
       }
+      console.log(selectedAnswer);
       return prevIndex + 1;
     });
-
-    
   }
 
+  function handleAnswer(answer) {
+    if (questions[currentQuestionIndex].correct_answer === (answer ? 'True' : 'False')) {
+      setScore(prevScore++);
+    }
+    handleNextQuestion();
+  }
+
+  function validateAnswer(){
+    if (selectedAnswer == null) {
+      alert('Veillez sélectionner une réponse !')
+    } else{
+      handleAnswer(selectedAnswer);
+      setSelectedAnswer(null)
+    }
+  }
 
   return (
     <div className="quiz-container">
       <h1>Quizmaster</h1>
-      {questions && questions.length > 1 && questions.map((quest, index) => (
+      {questions && questions.length > 1 && questions.map((currentQuestion, index) => (
         // Utilisation de la clé unique pour chaque élément
         <div key={index} style={{ display: index === currentQuestionIndex ? 'block' : 'none' }}>
-          <p dangerouslySetInnerHTML={{ __html: quest.question }}></p>
-          {console.log(quest.question)}
-          <div className="options">
-            <label>
-              <input
-                type="radio"
-                name="answer"
-                value="true"
-                checked={selectedAnswer === 'true'}
-                onChange={() => handleAnswerChange('true')}
-              />
-              Vrai
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="answer"
-                value="false"
-                checked={selectedAnswer === 'false'}
-                onChange={() => handleAnswerChange('false')}
-              />
-              Faux
-            </label>
-          </div>
+          <p dangerouslySetInnerHTML={{ __html: currentQuestion.question }}></p>
+          {console.log(currentQuestion.question)}
+          <button onClick={() =>{setSelectedAnswer(true)}}>Vrai</button>
+          <button onClick={() =>{setSelectedAnswer(false)}}>Faux</button>
         </div>
       ))}
-      <button onClick={handleNextQuestion}>Valider</button>
+      <button onClick={validateAnswer}>Valider</button>
     </div>
   );
 }
